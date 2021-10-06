fdf7b1e4e15a8c863e762c511c9d5c36c0c4e387
armanmasangkay <vss.arman@gmail.com>
2021-10-07 06:29:52 +0800

    fix indention issues

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -12,36 +12,21 @@ class PersonController extends Controller
 
     protected PersonRepository $repository;
 
-    public function __construct(PersonRepository $repository )
+    public function __construct(PersonRepository $repository)
     {
         $this->repository=$repository;  
     }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+  
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StorePersonRequest $request)
     {
         $this->repository->create($request->all());
@@ -49,46 +34,22 @@ class PersonController extends Controller
         return redirect(route('persons.index'))->with(['created' => true]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(UpdatePersonRequest $request, Person $person)
     {
         $this->repository->update($request->all(),$person);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy($id)
     {
         //
--- tests/Feature/UpdatePersonInformationTest.php
+++ tests/Feature/UpdatePersonInformationTest.php
@@ -21,99 +21,99 @@ class UpdatePersonInformationTest extends TestCase
         $response->assertForbidden();
     }
 
-      /** @test */
-      public function updateRouteIsAccessibleToAuthenticatedUsers()
-      {
-          $person=Person::factory()->create();
+    /** @test */
+    public function updateRouteIsAccessibleToAuthenticatedUsers()
+    {
+        $person=Person::factory()->create();
 
-          $user=User::factory()->create();
+        $user=User::factory()->create();
 
-          $this->actingAs($user);
+        $this->actingAs($user);
 
-          $response=$this->put(route('persons.update',$person),[
-              'firstname'=>'Arman',
-              'lastname'=>'Masangkay',
-              'middlename'=>'Macasuhot',
-              'address'=>'Malitbog'
-          ]);
-  
-          $response->assertOk();
-      }
+        $response=$this->put(route('persons.update',$person),[
+            'firstname'=>'Arman',
+            'lastname'=>'Masangkay',
+            'middlename'=>'Macasuhot',
+            'address'=>'Malitbog'
+        ]);
 
-      /** @test */
-      public function shouldUpdatePersonInformation()
-      {
-          $person=Person::factory()->create();
+        $response->assertOk();
+    }
 
-          $user=User::factory()->create();
+    /** @test */
+    public function shouldUpdatePersonInformation()
+    {
+        $person=Person::factory()->create();
 
-          $this->actingAs($user);
+        $user=User::factory()->create();
 
-          $response=$this->put(route('persons.update',$person),[
-              'firstname'=>'Arman',
-              'lastname'=>'Masangkay',
-              'middlename'=>'Macasuhot',
-              'address'=>'Malitbog'
-          ]);
-  
-          $updatedPerson=Person::find($person->id);
+        $this->actingAs($user);
 
-          $this->assertSame('Arman',$updatedPerson->firstname);
-          $this->assertSame('Masangkay',$updatedPerson->lastname);
-          $this->assertSame('Macasuhot',$updatedPerson->middlename);
-          $this->assertSame('Malitbog',$updatedPerson->address);
-      }
+        $response=$this->put(route('persons.update',$person),[
+            'firstname'=>'Arman',
+            'lastname'=>'Masangkay',
+            'middlename'=>'Macasuhot',
+            'address'=>'Malitbog'
+        ]);
+
+        $updatedPerson=Person::find($person->id);
+
+        $this->assertSame('Arman',$updatedPerson->firstname);
+        $this->assertSame('Masangkay',$updatedPerson->lastname);
+        $this->assertSame('Macasuhot',$updatedPerson->middlename);
+        $this->assertSame('Malitbog',$updatedPerson->address);
+    }
+
+    /** @test */
+    public function shouldNotUpdateIfRequiredFieldsAreEmpty()
+    {
+        $person=Person::factory()->create();
+
+        $user=User::factory()->create();
+
+        $this->actingAs($user);
+
+        $response=$this->put(route('persons.update',$person),[
+            'firstname'=>'',
+            'lastname'=>'',
+            'middlename'=>'Macasuhot',
+            'address'=>''
+        ]);
+
+        $response
+            ->assertRedirect(route('persons.edit',$person))
+            ->assertSessionHasErrors([
+                'firstname',
+                'lastname',
+                'address'
+            ]);
+    }
 
         /** @test */
-        public function shouldNotUpdateIfRequiredFieldsAreEmpty()
-        {
-            $person=Person::factory()->create();
-  
-            $user=User::factory()->create();
-  
-            $this->actingAs($user);
-  
-            $response=$this->put(route('persons.update',$person),[
-                'firstname'=>'',
-                'lastname'=>'',
-                'middlename'=>'Macasuhot',
-                'address'=>''
-            ]);
-    
-            $response
-                ->assertRedirect(route('persons.edit',$person))
-                ->assertSessionHasErrors([
-                    'firstname',
-                    'lastname',
-                    'address'
-                ]);
-        }
+    public function shouldNotBeAbleToUpdateUid()
+    {
+        $person=Person::factory()->create();
 
-           /** @test */
-      public function shouldNotBeAbleToUpdateUid()
-      {
-          $person=Person::factory()->create();
+        $user=User::factory()->create();
 
-          $user=User::factory()->create();
+        $this->actingAs($user);
 
-          $this->actingAs($user);
+        $response=$this->put(route('persons.update',$person),[
+            'uid'=>'123',
+            'firstname'=>'Arman',
+            'lastname'=>'Masangkay',
+            'middlename'=>'Macasuhot',
+            'address'=>'Malitbog'
+        ]);
 
-          $response=$this->put(route('persons.update',$person),[
-              'uid'=>'123',
-              'firstname'=>'Arman',
-              'lastname'=>'Masangkay',
-              'middlename'=>'Macasuhot',
-              'address'=>'Malitbog'
-          ]);
-  
-          $updatedPerson=Person::find($person->id);
+        $updatedPerson=Person::find($person->id);
 
-          $this->assertSame('Arman',$updatedPerson->firstname);
-          $this->assertSame('Masangkay',$updatedPerson->lastname);
-          $this->assertSame('Macasuhot',$updatedPerson->middlename);
-          $this->assertSame('Malitbog',$updatedPerson->address);
-          $this->assertNotSame('123',$updatedPerson->uid);
-      }
+        $this->assertSame('Arman',$updatedPerson->firstname);
+        $this->assertSame('Masangkay',$updatedPerson->lastname);
+        $this->assertSame('Macasuhot',$updatedPerson->middlename);
+        $this->assertSame('Malitbog',$updatedPerson->address);
+        $this->assertNotSame('123',$updatedPerson->uid);
+    }
 
 
 }
